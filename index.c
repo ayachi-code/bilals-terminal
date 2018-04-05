d337d5b84ce3bae08723768adc36e9c825d5804a
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-05 08:24:50 +0200

    Loopt het want dan kan je meer commandos doen i.p.v een

--- index.c
+++ index.c
@@ -11,10 +11,12 @@ int main(int argc, char **argv)
 
     //De input waarden met een buffer van 500
     char de_input_waarden[500];
-
+    //Loopt het want dan kan je meer commandos doen i.p.v een
+    while (1 == 1) {
     //De input display text dit is wat je er naast haat typen..
     printf("Bilal:shell>>> ");
     //De input word opgeslagen in de in put waarden
     scanf("%s",de_input_waarden);
+    }
     return 0;
 }
