a38ad203d8f2b98422d51b5c180a3fec1ea7a8a6
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-06 19:24:56 +0200

    Loopt door de hele bestand en slaat het op in de array gelezen bestand en print het uiteindelijk tot als het bestand eindigt.

--- commandos/lees.c
+++ commandos/lees.c
@@ -9,9 +9,16 @@ int lees(char naam[500]) {
 
     //Hier komt de context van het bestand en het word opgeslagen in gelezen_bestand...
     char gelezen_bestand[600];
-
     //Opent bestand
     FILE *bestand = fopen(naam,"r+");
+
+    //Loopt door de hele bestand en slaat het op in de array gelezen bestand en print het uiteindelijk tot als het bestand eindigt.
+    while(!feof(bestand)) {
+        fgets(gelezen_bestand,600,bestand);
+        puts(gelezen_bestand);
+    }
+
+
     //Sluit bestand
     fclose(bestand);
     return 0;
