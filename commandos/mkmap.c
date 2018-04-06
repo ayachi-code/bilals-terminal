d0160b76d6e4c7007a892a1897265a313c228932
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-06 11:34:52 +0200

    Mappen lib geimporteerd

--- commandos/mkmap.c
+++ commandos/mkmap.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 //invoer uitvoer lib
 #include <stdio.h>
-
+//Mappen lib geimporteerd
+#include <dirent.h>
 
 //Mkmap functie, met naam char variablen
 int mkmap(char naam[500]) {
