2a7d9fb3f555fba476d903cc8e3979323dd322df
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-06 11:36:23 +0200

    opent map en sluit map

--- commandos/mkmap.c
+++ commandos/mkmap.c
@@ -7,5 +7,9 @@
 
 //Mkmap functie, met naam char variablen
 int mkmap(char naam[500]) {
+    //Opent een map met parameter naam
+    DIR *mapnaam = opendir(naam);
+    //Sluit map
+    closedir(mapnaam);
     return 0;
 }
