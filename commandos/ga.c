acb64e50202ae126f529718a3f775fed6b45bbf1
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-06 19:50:21 +0200

    OS libary unix geimporteerd

--- commandos/ga.c
+++ commandos/ga.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 //invoer uitvoer lib
 #include <stdio.h>
+//OS libary unix geimporteerd
+#include <unistd.h>
 
 
-//Ga functie gemaakt
+//Ga functie gemaakt met argument als path locatie
 int ga(char locatie[500]) {
-
+    return 0;
 }
