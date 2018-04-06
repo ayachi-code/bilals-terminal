cacd8164e156e1cbe8ca5cff38d84c5b3119b5bf
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-06 17:33:28 +0200

    erwijderd map dan

--- index.c
+++ index.c
@@ -87,6 +87,8 @@ int main(int argc, char **argv)
         nieuwe_woord = strtok(de_input_waarden,karater);
         //Splits met NULL en spatie als gevolg de 2e parameter A.K.A de map 
         nieuwe_woord = strtok(NULL,karater);
+        //Verwijderd map dan
+        wgmap(nieuwe_woord);
     } else {
         //Als commando niet bestaat voer dit onder uit.....
         printf("Commando: %s bestaat niet :(  \n",de_input_waarden);
