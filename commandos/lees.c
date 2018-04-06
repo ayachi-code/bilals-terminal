8cbbc9e5a75c8a11ef60fb9d379ce947c0e9ffe4
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-06 19:21:42 +0200

    Hier komt de context van het bestand en het word opgeslagen in gelezen_bestand...

--- commandos/lees.c
+++ commandos/lees.c
@@ -6,8 +6,12 @@
 
 //Lees functie met argument naam
 int lees(char naam[500]) {
+
+    //Hier komt de context van het bestand en het word opgeslagen in gelezen_bestand...
+    char gelezen_bestand[600];
+
     //Opent bestand
-    FILE *bestand = fopen(naam,"w+");
+    FILE *bestand = fopen(naam,"r+");
     //Sluit bestand
     fclose(bestand);
     return 0;
